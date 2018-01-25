fded210e6a921721f93ec233473825295bfe06bd
Mitchell Stride <mitchellcstride@gmail.com>
2018-01-25 18:58:23 -0330

    Code post

---
+++ Propulsion_Stand_POC_Data_Capture/Propulsion_Stand_POC_Data_Capture.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // start switch when pulled low
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//Setup precise small timers so the data input is measured precisely for 2 seconds
