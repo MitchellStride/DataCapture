49627381383fc0d22648eaf963919b9cd78ccd45
Mitchell Stride <mitchellcstride@gmail.com>
2018-01-28 01:40:21 -0330

    added some tests to folder

--- Propulsion_Stand_POC_Data_Capture/Propulsion_Stand_POC_Data_Capture.ino
+++ Propulsion_Stand_POC_Data_Capture/Propulsion_Stand_POC_Data_Capture.ino
@@ -1,6 +1,23 @@
+#include "SD.h"
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+
+//SD Card Setup
+const int SDpin = 4;
+String dataString =""; // holds the data to be written to the SD card
+float sensorReading_Temp = 0.00; // value read from your first sensor
+float sensorReading_Pressure = 0.00; // value read from your second sensor
+File sensorData;
+
+// Thermocouple H/W SPI instance
+#define MAXCS   10
+Adafruit_MAX31855 thermocouple(MAXCS);
+
 // Setup startSwitch
 const int startSwitch = 2;
-int testLength = 0 // 4x2 = 8 seconds of data
+volatile int testLength = 0; // 4x2 = 8 seconds of data
+volatile int pressure;
+
 
 void setup() {
 cli();  
@@ -22,29 +39,62 @@ cli();
   TCCR1B |= (1 << CS12) | (1 << CS10); //Set CS10 and CS12 bits for 1024 prescaler
   TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
 
-//Wait to start data capture.
+//Configuring Pins
+Serial.begin(9600);
+Serial.print("Initializing SD card...");
+  if (!SD.begin(SDpin)) 
+    Serial.println("Card failed, or not present");
+pinMode(SDpin, OUTPUT);
 pinMode(startSwitch, INPUT_PULLUP);
+pinMode(A0, INPUT);
+Serial.println("card initialized.");
+Serial.println("Ready to begin test...");
 while(startSwitch == 1){}
+Serial.println("Input Received, Test begin..");
 sei();//allow interrupts
 }
-
 
 void loop() {
 }
 
-ISR(TIMER1_COMPA_vect){
-//check volatile
-//read thermocouple and pressure
-//sd card csv file
+ISR(TIMER0_COMPA_vect){
+  //read pressure
+  pressure = analogRead(A0);
+  pressure = map(pressure, 101, 920.7, 0, 2000);
+   if (isnan(pressure)) {
+    pressure = 0;
+   }
+
+  //read temperature
+  double temp = thermocouple.readCelsius();
+   if (isnan(temp)) {
+    temp = 0;
+   }
+   
+  //store data
+  dataString = String(sensorReading_Temp) + "," + String(sensorReading_Pressure); // convert to CSV
+  saveData(); // save to SD card
 }
 
 ISR(TIMER1_COMPA_vect){
 testLength++;
   if(testLength == 4)
-    cli;
+    cli();
+    Serial.println("Test Complete! Extract Data");
 //other code to end test
-    
 }
 
+void saveData(){
+  if(SD.exists("data.csv")){ // check the card is still there
+     sensorData = SD.open("data.csv", FILE_WRITE);
+      if (sensorData){
+         sensorData.println(dataString);
+         sensorData.close();
+      }
+  } 
+  else{
+    Serial.println("Error writing to file !");
+  }
+}
 
 
